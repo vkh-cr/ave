27b3e4ebb0683aa8abf6e13b722a13a725f4a5d0
Zdeněk Softič <zdenek@softic.cz>
2022-11-17 11:57:39 +0100

    Graphql libs updated, Graphql Auth, Debug toolbar

--- av_backend/av_backend/urls.py
+++ av_backend/av_backend/urls.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.urls import include, path
 from django.contrib import admin
 
-# from strawberry.django.views import GraphQLView
 from strawberry_django_jwt.views import StatusHandlingGraphQLView as GraphQLView
 from strawberry_django_jwt.decorators import jwt_cookie
 
@@ -33,9 +32,6 @@ if settings.DEBUG:
     ]
 
 urlpatterns = urlpatterns + [
-    # For anything not caught by a more specific rule above, hand over to
-    # Wagtail's page serving mechanism. This should be the last pattern in
-    # the list:
     path("", include(wagtail_urls)),
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:
